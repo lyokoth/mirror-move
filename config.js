8072def257b38e497cf0d53d5c4f27585119d99c
Lynette Okoth <lynokoth79@gmail.com>
2025-05-18 16:20:49 -0400

    .

--- config.js
+++ config.js
@@ -1,7 +1,23 @@
+const Discord = require('discord.js');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+    ]
+});
+
+
 module.exports = {
 
     USERNAME: 'showdisdisk',
     PASSWORD: 'VATONAGE',
-    FORMAT: "gen9randombatte"
+    FORMAT: "gen9randombattle",
+    SHOWDOWN_SERVER: 'wss://sim.smogon.com:443/showdown/websocket',
+   
 
 };
