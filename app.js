991be59ad64d48dac802640194b60955dc86ef82
Lynette Okoth <lynokoth79@gmail.com>
2025-05-18 16:20:21 -0400

    merge index and app.js into app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import discord, { DMChannel } from 'discord.js'; 
-import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits, REST, Routes, Collection } from 'discord.js';
 import { USERNAME, PASSWORD, FORMAT } from './config.js';
 import { createRequire } from 'module';
+i    
 
+const axios = require('axios');
+const utils = require('./utils.js');
+const ReplayTracker = require('./tracker/ReplayTracker'); // use replay traacker to learn plays 
 const bot = new discord.Client({
     fetchAllMembers: true,
     commands: new discord.Collection(),
@@ -15,21 +19,43 @@ const bot = new discord.Client({
     ]
 
 });
+['commands'].forEach((x) => (bot[x] = new Collection()));
+// Setting up process.env\
 
 const rest = REST({version: '10'}).setToken(TOKEN);
+
+//
 
 // login function
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.on(Events.ClientReady, readyClient => {
+client.ws.on(Events.ClientReady, readyClient => {
     console.log(`Logged in as ${readyClient.user.tag}!`);
 });
-client.on(Events.InteractionCreate, interaction => {
+client.ws.on(Events.InteractionCreate, async (interaction) => {
+    let link = interaction.data.options[0].value + ".log";  
+    let response = await axios
+        .get(link, {
+            headers: { "User-Agent": "ShowdownBot" },
+        })
+        .catch((e) => console.error(e));
+    let data = response.data;
+
+
+
+
     if (interaction.isCommand()) {
         const { commandName } = interaction;
         if (commandName === 'ping') {
-            interaction.reply('Pong!');
+            interaction.reply('Pong!'); 
         }
     }
 });
+
+client.once(Events.ClientReady, () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('Pokémon Showdown', { type: 'WATCHING' });
+});
+
+
 client.login(TOKEN);
