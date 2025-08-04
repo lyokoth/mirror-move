4fe23d9c566f7803dfba1e872fbcf5a3885a4377
Lynette Okoth <lynokoth79@gmail.com>
2025-08-04 19:10:44 -0400

    Update monoTeam.js

--- commands/monoTeam.js
+++ commands/monoTeam.js
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { infoEmbed, errorEmbed } from '../pages/module.js';
-import { parseMonoType } from '../utils/pokeUtils.js';
-import { autoCompletePokemon, cleanPokemonName } from '../utils/module.js';
+const { infoEmbed, errorEmbed } = require('../pages/module');
+const { autoCompletePokemon, cleanPokemonName, parseMonoType } = require('../utils/module');
 
 
 module.exports = {
@@ -15,9 +14,23 @@ module.exports = {
                 .setAutocomplete(true)
         )
         ,
+        // Randomly generate a team by type (and gen if specified)
+    async autocomplete(interaction) {
+        await autoCompletePokemon(interaction);
+    }
+    ,
     async execute(interaction) {
-        const input = interaction.options.getString('input');
-        const types = parseMonoType(input);
-        // Further processing with the types
+        const type = interaction.options.getString('input');
+        const types = parseMonoType(type);
+        if (types.length === 0) {
+            return await interaction.reply(errorEmbed('Invalid type input.'));
+        }
+        
+        try {
+            const embed = await infoEmbed(types[0], 9); // Assuming gen 9 for mono-type
+            await interaction.reply(embed);
+        } catch (err) {
+            await interaction.reply(errorEmbed(err));
+        }
     }
 };
