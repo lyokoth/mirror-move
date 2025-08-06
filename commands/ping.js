38fd8fd8bb62b2389dbcc10cc32234af3bdcf14b
Lynette Okoth <lynokoth79@gmail.com>
2025-08-05 20:49:59 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,19 +2,26 @@ import { SlashCommandBuilder } from 'discord.js';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('ding')
+    .setDescription("dong! let's battle!"),
 
-export default { 
-	data: new SlashCommandBuilder()
-		.setName('ding')
-		.setDescription('dong! let`s battle, `${username}`!'),
-	async execute(interaction) {
-		const markup = await axios.get('https://play.pokemonshowdown.com/sprites/gen5/');
-		const $ = cheerio.load(markup.data);
-		const parsedTeam = $('a').text().split('.png');
-		parsedTeam.forEach(element => {
-			if ((element.includes('urshifu') || element.includes('galar') && !element.includes('shiny'))) console.log('`' + element + '`,');
-		});
-		console.log(parsedTeam.length);
-		await interaction.reply('Pong!');
-	},
-};
+  async execute(interaction) {
+    const markup = await axios.get('https://play.pokemonshowdown.com/sprites/gen5/');
+    const $ = cheerio.load(markup.data);
+  
+
+  const links = [];
+  $('a').each((i, el) => {
+    const href = $(el).attr('href');
+    if (href && (href.includes('urshifu') || href.includes('galar')) && !href.includes('shiny')) {
+      console.log(`\`${href}\`,`);
+      links.push(href);
+    }
+  });
+
+  console.log(`Total matching links: ${links.length}`);
+  await interaction.reply(`dong! let's battle, ${interaction.user.username}!`);
+}
+} 
